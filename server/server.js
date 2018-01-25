81370045543efd8369703e9fa757e32ff64f9289
intothedeep <victor.taek.lim@gmail.com>
2018-01-25 23:23:04 +0900

    port change

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log("dir == " + __dirname);
